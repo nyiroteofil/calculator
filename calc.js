a10674e8919609115621b0dc17a10cb5cc6bd111
NyirőTeofil <portrider01234@gmail.com>
2022-09-15 14:43:41 +0200

    formated code

--- calc.js
+++ calc.js
@@ -17,168 +17,168 @@ let equalPressed = false;
 let operatorPressed = false;
 let floatPressed = false;
 
-    function convert(n) {
+function convert(n) {
 
-        if (n.includes('.')) {
+    if (n.includes('.')) {
 
-            return parseFloat(n);
-        
-        } else {
-        
-            return parseInt(n);
-        
-        }
-    
+        return parseFloat(n);
+
+    } else {
+
+        return parseInt(n);
+
     }
 
-    function calculate(op, first, second) {
+}
 
-    switch(op) {
+function calculate(op, first, second) {
+
+    switch (op) {
         case 'add': return first + second;
-        break;
-        case 'substract' : return first - second;
-        break;
-        case 'multiply' : return first * second;
-        break;
-        case 'divide' : return first / second;
-        break;
-        default : return op;
-        break;
+            break;
+        case 'substract': return first - second;
+            break;
+        case 'multiply': return first * second;
+            break;
+        case 'divide': return first / second;
+            break;
+        default: return op;
+            break;
     }
 }
 
-    /*Listeners for the buttons*/
+/*Listeners for the buttons*/
 
-    buttons.forEach((c) => {
-        c.addEventListener('click', (n) => {
+buttons.forEach((c) => {
+    c.addEventListener('click', (n) => {
 
-            if (display.textContent === '0') {
+        if (display.textContent === '0') {
 
-                display.textContent = n.target.textContent;
-            
-            } else if (display.textContent[0] === '.') {
+            display.textContent = n.target.textContent;
 
-                display.textContent.shift('0')
-            
-            } else if (equalPressed === true){
+        } else if (display.textContent[0] === '.') {
 
-                display.textContent = n.target.textContent;
+            display.textContent.shift('0')
 
-                
-                equalPressed = false;
+        } else if (equalPressed === true) {
 
-            } else if (operatorPressed === true) {
-
-                display.textContent = n.target.textContent;
-
-                operatorPressed = false;
-
-            } else {
-
-                display.textContent += n.target.textContent;
-
-            }
-
-        });
-    });
+            display.textContent = n.target.textContent;
 
 
-    operators.forEach((c) => {
-        c.addEventListener('click', (n) => {
+            equalPressed = false;
 
-            operators.forEach((t) => {
+        } else if (operatorPressed === true) {
 
-                t.style.backgroundColor = 'rgb(228, 56, 13)';
-            
-            });
+            display.textContent = n.target.textContent;
 
-            n.target.style.backgroundColor = 'rgb(17, 211, 211)';
-
-            op = n.target.id;
-
-            if (typeof prev === 'undefined') {
-
-                prev = convert(display.textContent);
-            
-            } else if ( typeof prev !== 'null' && typeof nxt === 'undefined') {
-    
-                nxt = convert(display.textContent);
-
-                prev = calculate(prevOP, prev, nxt);
-
-                display.textContent = prev;
-            
-            } else {
-
-                console.log(prev);
-
-                nxt = convert(display.textContent);
-
-                prev = calculate(prevOP, prev, nxt);
-
-                display.textContent = prev;
-    
-            }
-
-            operatorPressed = true;
-            floatPressed = false;
-
-            prevOP = op;
-        })  
-    });
-
-    clear.addEventListener('click', () => {
-
-        prevOP = undefined;
-        op = undefined;
-
-        prev = undefined;
-        nxt = undefined;
-
-        equalPressed = false;
-        operatorPressed = false;
-        floatPressed = false;
-
-        display.textContent = '0';
-        
-    });
-
-    equals.addEventListener('click', () => {
-        
-        if ( typeof prev === 'undefined') {
-            
-            display.textContent = '0';
+            operatorPressed = false;
 
         } else {
-            
-            display.textContent = calculate(op, prev, convert(display.textContent));
-        
+
+            display.textContent += n.target.textContent;
+
         }
 
-        equalPressed = true;
-        operatorPressed = false;
-        floatPressed = false,
+    });
+});
 
-        prev = undefined;
-        nxt = undefined;
-        op = undefined
-        
+
+operators.forEach((c) => {
+    c.addEventListener('click', (n) => {
+
         operators.forEach((t) => {
 
             t.style.backgroundColor = 'rgb(228, 56, 13)';
-        
-        }); 
+
+        });
+
+        n.target.style.backgroundColor = 'rgb(17, 211, 211)';
+
+        op = n.target.id;
+
+        if (typeof prev === 'undefined') {
+
+            prev = convert(display.textContent);
+
+        } else if (typeof prev !== 'null' && typeof nxt === 'undefined') {
+
+            nxt = convert(display.textContent);
+
+            prev = calculate(prevOP, prev, nxt);
+
+            display.textContent = prev;
+
+        } else {
+
+            console.log(prev);
+
+            nxt = convert(display.textContent);
+
+            prev = calculate(prevOP, prev, nxt);
+
+            display.textContent = prev;
+
+        }
+
+        operatorPressed = true;
+        floatPressed = false;
+
+        prevOP = op;
+    })
+});
+
+clear.addEventListener('click', () => {
+
+    prevOP = undefined;
+    op = undefined;
+
+    prev = undefined;
+    nxt = undefined;
+
+    equalPressed = false;
+    operatorPressed = false;
+    floatPressed = false;
+
+    display.textContent = '0';
+
+});
+
+equals.addEventListener('click', () => {
+
+    if (typeof prev === 'undefined') {
+
+        display.textContent = '0';
+
+    } else {
+
+        display.textContent = calculate(op, prev, convert(display.textContent));
+
+    }
+
+    equalPressed = true;
+    operatorPressed = false;
+    floatPressed = false,
+
+        prev = undefined;
+    nxt = undefined;
+    op = undefined
+
+    operators.forEach((t) => {
+
+        t.style.backgroundColor = 'rgb(228, 56, 13)';
 
     });
 
-    floatingPoint.addEventListener('click', () => {
+});
 
-        if (floatPressed === false) {
+floatingPoint.addEventListener('click', () => {
 
-            display.textContent += '.';
-        
-        }
+    if (floatPressed === false) {
 
-        floatPressed = true;
+        display.textContent += '.';
 
-    })
+    }
+
+    floatPressed = true;
+
+})
